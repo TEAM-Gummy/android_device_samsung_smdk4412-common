19e988fe139639db192838e76cc14bb535c7332a
hedwig34 <hedwig34@gmail.com>
2014-07-10 07:46:55 -0400

    smdk4412-common: TWRP-ify
    Change-Id: I6319bbf079c735fc89283469f0d0318e6699118a

--- common.mk
+++ common.mk
@@ -34,6 +34,10 @@ PRODUCT_COPY_FILES := \
     $(COMMON_PATH)/rootdir/ueventd.smdk4x12.rc:root/ueventd.smdk4x12.rc \
     $(COMMON_PATH)/rootdir/ueventd.smdk4x12.rc:recovery/root/ueventd.smdk4x12.rc
 
+# TWRP
+PRODUCT_COPY_FILES := \
+    $(COMMON_PATH)/recovery/twrp.fstab:recovery/root/etc/twrp.fstab
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/audio_effects.conf:system/etc/audio_effects.conf \
